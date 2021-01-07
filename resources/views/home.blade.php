ba262f6f58d3e0ff21021869a12180d9e710257b
Daniel Tutistar <danieltutistar@gmail.com>
2021-01-07 02:31:56 -0500

    pagina de inicio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('Layouts.master-templete');
+@section('titulo')
+    Inicio
+@endsection
+@section('contenido')
+<h1>Aqui pondria mi proyecto de Legal y Etica ... <b>Si tuviera uno!</b></h1>
+@endsection
