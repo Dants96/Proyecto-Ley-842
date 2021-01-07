8c17da98bee98d6f72a4b9c49a96740dcf96ed43
Daniel Tutistar <danieltutistar@gmail.com>
2021-01-07 18:41:21 -0500

    modelos de base de datos creadas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Administrador;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+});
+
+Route::get('/modeltest', function(){
+    $datos = Administrador::all();
+    echo($datos);
+    dd($datos);
+    
 });
