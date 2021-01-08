f120f42b8a39d08957aa4ce86eb3cf5ee97fe23d
Daniel Tutistar <danieltutistar@gmail.com>
2021-01-07 22:15:33 -0500

    master template terminado

--- resources/views/Layouts/master-templete.blade.php
+++ resources/views/Layouts/master-templete.blade.php
@@ -1,16 +1,127 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <title>App Name - @yield('titulo')</title>
-    <style>@yield('style')</style>
+    <style>
+    </style>
 </head>
+
 <body>
-    <div class="container">
-        @yield('contenido')
+    <div class="wrapper">
+        <!-- Sidebar Holder -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Ley 842 de 2013</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>Menu de Navegacion</p>
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Indices</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="#">Títulos </a>
+                        </li>
+                        <li>
+                            <a href="#">Capítulos</a>
+                        </li>
+                        <li>
+                            <a href="#">Artículos</a>
+                        </li>
+                    </ul>
+                </li>
+                <li> 
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false"
+                        class="dropdown-toggle">Etiquetas</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="#">Page 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Info Ley 842</a>
+                    <a href="#">Contactanos</a>
+                </li>
+            </ul>
+
+            <ul class="list-unstyled CTAs">
+                <li>
+                    <a href="https://www.copnia.gov.co/" class="download" target="_blank">COPNIA</a>
+                </li>
+                <li>
+                    <a href="{{route('inicio')}}" class="article">Administrador</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- Page Content Holder -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="navbar-btn">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="{{route('inicio')}}">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Info</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Administrador</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="container">
+                @yield('contenido')
+            </div>
+           
+        </div>
     </div>
+
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+                $(this).toggleClass('active');
+            });
+        });
+
+    </script>
+    
 </body>
+
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,18 @@
-@extends('Layouts.master-templete');
+@extends('Layouts.master-templete')
 @section('titulo')
     Inicio
 @endsection
 @section('contenido')
 <h1>Aqui pondria mi proyecto de Legal y Etica ... <b>Si tuviera uno!</b></h1>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra semper nisi sed scelerisque. Aliquam eget mauris sed ex ullamcorper rhoncus. Nullam tincidunt fringilla lorem in pharetra. Donec suscipit, diam vitae interdum posuere, orci dui sagittis diam, ut feugiat nisi odio efficitur lorem. Sed iaculis neque ac imperdiet ullamcorper. Nullam auctor tellus velit, vel hendrerit arcu lobortis fringilla. Sed eu consectetur lorem. Vestibulum vitae lacus ante. Morbi rutrum feugiat erat, vitae consectetur ipsum varius vel. Suspendisse ac faucibus dolor.
+
+Aliquam hendrerit lacus vitae blandit tempus. Fusce maximus, massa id semper pellentesque, elit metus tincidunt magna, at malesuada augue nunc non neque. Phasellus efficitur odio non nulla suscipit interdum. In justo sem, fermentum pellentesque euismod eget, lobortis quis dui. Sed bibendum sapien non risus volutpat, a dapibus ante fringilla. Praesent ultrices velit vitae hendrerit maximus. Proin blandit eros ac nisl interdum, at ultricies elit consectetur. Sed ac dignissim sapien. Duis eu euismod urna. Cras et placerat mauris. Ut euismod leo enim, sodales imperdiet eros congue sed. Suspendisse quis molestie lorem, et porta eros. Donec vel purus a massa posuere placerat consectetur vitae nulla. Quisque sem justo, finibus ac quam in, efficitur iaculis eros.
+
+Vestibulum dapibus id lectus eu scelerisque. Nulla facilisi. Etiam facilisis neque sit amet dui efficitur, et mollis risus venenatis. Quisque faucibus efficitur ex, nec dapibus velit. Etiam lacinia laoreet risus in molestie. Nunc at elementum nibh. Suspendisse a dolor lacinia, tempus eros eu, tempus justo. Fusce at nisl in arcu ultrices pretium sed sed tortor. Integer porta mattis enim, et iaculis nunc malesuada a.
+
+Nullam nec massa enim. Praesent odio nisl, tincidunt quis rutrum ut, luctus vitae augue. Maecenas eu pharetra libero, vitae pharetra neque. Vivamus eget felis ac est iaculis pellentesque sed et mauris. Integer erat metus, scelerisque euismod dui at, mollis fermentum sem. Suspendisse potenti. Donec nec ullamcorper ipsum, et commodo justo.
+
+Etiam eleifend feugiat quam, at placerat odio. Ut egestas sodales tellus, non sagittis lectus tincidunt a. Nullam ut ultricies enim. Etiam rutrum sem nec cursus vulputate. In et consectetur purus, a blandit eros. Phasellus mauris felis, suscipit vel nisi in, elementum rutrum augue. Nunc aliquet nunc purus, id aliquet lacus viverra non. Integer suscipit, lectus ac sodales egestas, augue magna aliquet turpis, nec cursus libero orci vitae lacus. Cras pellentesque accumsan metus, sit amet hendrerit turpis molestie a. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum eu lacinia eros, in bibendum ex. Etiam ante libero, lacinia ut aliquam ut, tincidunt eu leo. Sed nec augue diam.
+</p>
 @endsection
