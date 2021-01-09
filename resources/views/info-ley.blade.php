b5acafbb723ead9037606c358bbd03b8673274fc
Daniel Tutistar <danieltutistar@gmail.com>
2021-01-09 18:56:37 -0500

    sombra estandar en vista info

--- resources/views/info-ley.blade.php
+++ resources/views/info-ley.blade.php
@@ -3,7 +3,7 @@
     ley 842 de 2003
 @endsection
 @section('contenido')
-    <div class="slideDown">
+    <div class="slideDown shadow-std ">
         <div class="card">
             <iframe src="https://web.archive.org/web/20210109190245/https://sintesis.colombiacompra.gov.co/content/ley-842-de-2003" style="height:500px" id="inneriframe"></iframe>
             
